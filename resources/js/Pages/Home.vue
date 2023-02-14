bac6bdabb932cde5e1b30dec0454f5279a572417
Rico Pfister <contact.rico.pfister@gmail.com>
2023-02-14 06:56:26 +0100

    change home screen warning text: proof of cencept testing.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,8 +3,8 @@
     <Header>
         <div class="flex flex-col h-full justify-center items-center xl:mt-0 xl:pb-10">
             <div class="h-16 w-full text-center justify-center flex items-center text-xl md:text-3xl text-white bg-red-400 rounded-3xl mb-3 xl:mb-5 flex-col md:flex-row">
-                <div class="whitespace-nowrap">Beta Version.&nbsp;</div>
-                <div class="whitespace-nowrap">This site is under construction.</div>
+                <div class="whitespace-nowrap">This site is under construction - &nbsp;</div>
+                <div class="whitespace-nowrap">"Proof of Concept" Testing.</div>
             </div>
             <div class="w-full flex h-full flex-wrap justify-center 2xl:gap-24 p-7 bg-gray-200 rounded-3xl shadow-xl">
                 <img class="order-3 2xl:order-1" src="/storage/home/rico_assistant_home_image.svg">
